16b14145e8b66626d83a6c400fe9ab21c4717121
KajtiChr <kajchrzanowski@gmail.com>
2023-01-25 20:35:13 +0100

    Carousel workout

---
+++ src/layouts/HomePage/ExploreTopBoooks.tsx
@@ -0,0 +1,14 @@
+export const ExploreTopBooks = () =>{
+    return(
+        <div className='p-5 mb-4 bg-darr header'>
+            <div className="container-fluid py-5 text-white 
+                d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Find you next adventure</h1>
+                    <p className='col-md-8 fs-4'>Where woudl you like to go next?</p>
+                    <a type='button' className="btn main-color btn-lg text-white" href='#'>Exoplore top books</a>
+                </div>
+            </div>
+        </div>
+    );
+}
